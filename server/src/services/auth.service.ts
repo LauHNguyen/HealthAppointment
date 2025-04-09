e33556f35e01f99b6e1e19cbaacb0976e5c9ea96
LauHNguyen <hoannguyen.lau@gmail.com>
2025-04-09 14:55:11 +0700

    FIX - auth.service logic check usename length

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -29,6 +29,9 @@ export class AuthService {
     if (!password) {
       throw new Error('Password is blank');
     }
+    if(username.length < 4 || username.length > 20) {
+      throw new Error('Username must be between 4 and 20 characters long');
+    }
     if (password.length < 8) {
       throw new Error('Password must be at least 8 characters long');
     }
